0ad86f293f52bebb4599138c758ab66878e31ecd
Anth3995 <morenaleksey@gmail.com>
2022-02-10 19:51:09 +0200

    init project

---
+++ src/main/java/ua/com/yummy/YummyApplication.java
@@ -0,0 +1,12 @@
+package ua.com.yummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/com/yummy/YummyApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.yummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
